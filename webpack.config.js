aeafb49a63b26f3733f2ef1719bfeef11df5f93c
Karthik <35562287+Karthik-B-06@users.noreply.github.com>
2018-07-02 17:42:35 +0530

    Add files via upload
    React Boiler Plate

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require("path");
+
+module.exports = {
+  // Sets the Entry Point of the App
+  entry: {
+    app: './src/app.js',
+  },
+  // The output file of the Complete App
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  devtool: 'inline-cheap-source-map',
+  // webpack-dev-server provides you with a simple web server 
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx']
+  },
+  module : {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js?$/,
+        exclude: /(node_modules)/
+      }, {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }  
+};
